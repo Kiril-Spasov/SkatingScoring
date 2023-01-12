fb2c5134b1a59a147d9b98ceaa1c4e7626c1e6d9
Kiril Spasov <spasov.kiril@gmail.com>
2023-01-12 11:50:16 +0200

    Add project files.

---
+++ SkatingScoring/FormSkating.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace SkatingScoring
+{
+    partial class FormSkating
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCalculateScore = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnCalculateScore
+            // 
+            this.BtnCalculateScore.Location = new System.Drawing.Point(61, 56);
+            this.BtnCalculateScore.Name = "BtnCalculateScore";
+            this.BtnCalculateScore.Size = new System.Drawing.Size(177, 58);
+            this.BtnCalculateScore.TabIndex = 0;
+            this.BtnCalculateScore.Text = "Calculate Score";
+            this.BtnCalculateScore.UseVisualStyleBackColor = true;
+            this.BtnCalculateScore.Click += new System.EventHandler(this.BtnCalculateScore_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(273, 56);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(441, 341);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormSkating
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnCalculateScore);
+            this.Name = "FormSkating";
+            this.Text = "Skating Scoring";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCalculateScore;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ SkatingScoring/FormSkating.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SkatingScoring
+{
+    public partial class FormSkating : Form
+    {
+        public FormSkating()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculateScore_Click(object sender, EventArgs e)
+        {
+            string skaterName;
+            double[] scores = new double[12];
+
+            string path = Application.StartupPath + @"\skating.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            int pairsCount = Convert.ToInt32(streamReader.ReadLine());
+
+            TxtResult.Text += "Final results: " + Environment.NewLine;
+
+            for (int i = 1; i <= pairsCount; i++)
+            {
+                skaterName = streamReader.ReadLine();
+
+                for (int j = 0; j <= 11; j++)
+                {
+                    scores[j] = Convert.ToDouble(streamReader.ReadLine());
+                }
+
+                TxtResult.Text += skaterName + ": " + CalculateAverage(scores).ToString("n2") + Environment.NewLine;
+            }
+        }
+
+        private double CalculateAverage(double[] scores)
+        {
+            double totalOf10Scores = 0;
+            double average;
+
+            Sort(scores);
+
+            for (int i = 1; i < scores.Length - 1; i++)
+            {
+                totalOf10Scores += scores[i];
+            }
+
+            return average = totalOf10Scores / 10;
+
+        }
+
+        private void Sort(double[] numbers)
+        {
+            int minLocation;
+            double temp;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                minLocation = i;
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[minLocation] > numbers[j])
+                    {
+                        minLocation = j;
+                        
+                    }
+                }
+                temp = numbers[minLocation];
+                numbers[minLocation] = numbers[i];
+                numbers[i] = temp;
+            }
+        }
+    }
+}
